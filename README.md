bcf6dacc95c08c3fe3cc4d65789cbf6306667b74
ABdul Malik <is-amsackey3722@st.umat.edu.gh>
2024-10-20 20:10:43 -0700

    added a gui to makke it simple,comes with a button that invokes the screenshot and a button that closs it.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 This is a simple Python application that captures a screenshot after a delay and saves it as an image file. The app is built using pyautogui and Pillow libraries, and the project is managed within a virtual environment.
 FEATURES: Takes a screenshot after a 5-second delay,Saves the screenshot as test.png in the working directory
 Displays the screenshot once captured.
+--steps to use it
+1.clone repo
+2.pip install  virtualenv
+3.virtualenv env
+4. .env\Scripts\activate
+deactivate after running code (deactivate)
+nb add virtualenv to path after installation 
+export PATH=$PATH:/c/Users/Abdul\ Malik/AppData/Roaming/Python/Python312/Scripts/
+change the Abdul Malik with your username.
+ 
--- screenshot/screen.py
+++ screenshot/screen.py
@@ -1,12 +1,25 @@
 import time
-import pyautogui
-
+import pyautogui as ss
+import tkinter as tk
 
 def ScreenShot():
-    time.sleep(5)
+    # time.sleep(5)
     name=int(round(time.time()*100))
-    name='{}.png'.format(name)
-    img=pyautogui.screenshot(name)
+    name='C://Users//Abdul Malik//Desktop//PYTHON//screenshots{}.png'.format(name)
+    img=ss.screenshot(name)
     img.show()
     
-ScreenShot()
+def quit():
+    window.destroy()
+
+
+window=tk.Tk()
+window.title("Screenshot App")
+frame=tk.Frame(window)
+frame.pack()
+button=tk.Button(frame, text="take screenshot",command=ScreenShot)
+button.pack(side=tk.LEFT)
+close=tk.Button(frame,text="QUIT",command=quit)
+close.pack(side=tk.LEFT)
+
+window.mainloop()
